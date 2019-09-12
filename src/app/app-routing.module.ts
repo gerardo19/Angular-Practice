79d548b43a172f5df6c97be7b46dea8098949e3b
Leylin Esquivel <lesquivel@impesagroup.corp>
2019-09-12 09:00:43 -0600

    proloding strategy v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { BFlexboxComponent } from './Components/b-flexbox/b-flexbox.component';
 import { DirectivesComponent } from './Components/directivesComponent/directives.component';
+import { PreloadingStrategyService } from './services/preloading-strategy.service';
 
 const routes: Routes = [
   {
     path: 'customers',
-    loadChildren: './customers/customers.module#CustomersModule'
+    loadChildren: './customers/customers.module#CustomersModule',
+    data: { preload: true }
   },
   {
     path: 'orders',
@@ -27,14 +29,25 @@ const routes: Routes = [
   }
 ];
 
+/* Para precargar todos los módulos
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    /*
-    * Para precargar los modulos, luego de que ya se cargó toda la aplicación.
-      https://medium.com/@adrianfaciu/custom-preloading-strategy-for-angular-modules-b3b5c873681a
-    */
+    
+     //Para precargar los modulos, luego de que ya se cargó toda la aplicación.
+      //https://medium.com/@adrianfaciu/custom-preloading-strategy-for-angular-modules-b3b5c873681a
+    enableTracing: true,   //  <---- debuggin purposes only
     preloadingStrategy: PreloadAllModules
   })],
   exports: [RouterModule]
+})*/
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing: true,   //  <---- debuggin purposes only
+    preloadingStrategy: PreloadingStrategyService
+  })],
+  exports: [RouterModule]
 })
+
+
 export class AppRoutingModule { }
