f2ea4d5df92c69756f930f788d5f96fb4fbcea10
gerardo19 <lgerardoeb19@gmail.com>
2019-09-11 20:48:12 -0600

    Lazy modules and UI updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BFlexboxComponent } from './Components/b-flexbox/b-flexbox.component';
+import { DirectivesComponent } from './Components/directivesComponent/directives.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'customers',
+    loadChildren: './customers/customers.module#CustomersModule'
+  },
+  {
+    path: 'orders',
+    loadChildren: './orders/orders.module#OrdersModule'
+  },
+  {
+    path: 'flex',
+    component: BFlexboxComponent
+  },
+  {
+    path: 'directive',
+    component: DirectivesComponent
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
