992700ab9291f5fcb770538ede25132c598a61f1
gerardo19 <lgerardoeb19@gmail.com>
2019-09-11 20:57:12 -0600

    New comment to preloding strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { BFlexboxComponent } from './Components/b-flexbox/b-flexbox.component';
 import { DirectivesComponent } from './Components/directivesComponent/directives.component';
 
@@ -28,7 +28,13 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    /*
+    * Para precargar los modulos, luego de que ya se cargó toda la aplicación.
+      https://medium.com/@adrianfaciu/custom-preloading-strategy-for-angular-modules-b3b5c873681a
+    */
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
